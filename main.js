73d347aea21832bb4722177152c661f08fec21aa
AhmedERRAJAAI <ahmed.errajaai@gmail.com>
2021-10-18 13:08:43 +0100

    commiting style changes & fixing hours hand problem (17 oct 2021)

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ let secondsHand = document.querySelector('body > div > div.hand.second');
 
 function setClock(){
     const currentDate = new Date();
-    const hoursRatio = currentDate.getHours()/28;
+    let hoursRatio = currentDate.getHours();
+    hoursRatio = hoursRatio >= 12 ? hoursRatio - 12 : hoursRatio;
     const minutesRatio = currentDate.getMinutes()/60;
     const secondsRatio = currentDate.getSeconds()/60;
     setRotation(hoursHand,hoursRatio);
     setRotation(minutesHand,minutesRatio);
     setRotation(secondsHand,secondsRatio);
-    
 }
 function setRotation(elemnt,rotationValue){
     elemnt.style.setProperty('--rotation',rotationValue * 360);
