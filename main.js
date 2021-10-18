e3f9782d1f66749e0e4241320ccdb6af2cd691d2
AhmedERRAJAAI <ahmed.errajaai@gmail.com>
2021-10-18 15:26:22 +0100

    addling numeric clock

--- main.js
+++ main.js
@@ -20,3 +20,30 @@ function setRotation(elemnt,rotationValue){
     elemnt.style.setProperty('--rotation',rotationValue * 360);
 }
 setClock();
+
+/* -----    end of analog clock-------- */
+/* -----    start of numeric clock-------- */
+
+
+setInterval(setClockNumeric,1000);
+const hoursDisplay = document.querySelector('body > div.analogClock > div.aHours')
+const minutesDisplay = document.querySelector('body > div.analogClock > div.aMinutes')
+const secondsDisplay = document.querySelector('body > div.analogClock > div.aSeconds')
+
+function setClockNumeric(){
+  const  currentDateValue = new Date();
+  let hoursValue = currentDateValue.getHours().toString();
+  hoursValue = hoursValue.length<2? "0"+hoursValue:hoursValue;
+  let minutesValue = currentDateValue.getMinutes().toString();
+  minutesValue = minutesValue.length<2? "0"+minutesValue:minutesValue;
+    let secondsValue = currentDateValue.getSeconds().toString();
+    secondsValue = secondsValue.length<2? "0"+secondsValue:secondsValue;
+
+    secondsValue = ": "+secondsValue;
+    minutesValue = ": "+minutesValue;
+
+    hoursDisplay.textContent = hoursValue;
+    minutesDisplay.textContent = minutesValue;
+    secondsDisplay.textContent = secondsValue;
+}
+setClockNumeric();
